0c3cb615302a5640c9ab39d828028a5286511519
berramou <azedine.berramou@gmail.com>
2020-03-31 21:31:51 +0200

    Custom index css and js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import AppContainer from './components/AppContainer';
 import * as serviceWorker from './serviceWorker';
 
+const PRODUCTS = [
+  { ref: '4141589679', price: 49.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'Football', description: 'Football product description' },
+  { ref: '5766031063', price: 9.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'Baseball', description: 'Baseball product description' },
+  { ref: '0137738001', price: 29.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'Basketball', description: 'Basketball product description' },
+  { ref: '0593792626', price: 99.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'iPod Touch', description: 'iPod Touch product description' },
+  { ref: '4363988790', price: 399.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'iPhone 5', description: 'iPhone 5 product description' },
+  { ref: '5933973530', price: 199.99, image: 'https://dummyimage.com/300x200/808080/000000', name: 'Nexus 7', description: 'Nexus 7 product description' }
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="container">
+  <AppContainer products={PRODUCTS}/>
+  </div>
+ ,
   document.getElementById('root')
 );
 
